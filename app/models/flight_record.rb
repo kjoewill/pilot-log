ef373c597987a3ff97982686374f8e077b1d2a94
github username <github email address>
2018-11-19 14:24:45 -0700

    created seeds.rb and lightly tested models w/ tux

--- app/models/flight_record.rb
+++ app/models/flight_record.rb
@@ -1,5 +1,5 @@
 class FlightRecord < ActiveRecord::Base
   belongs_to :user
-  validates_presence_of :date, :aircraft, :from, :to, :num_landings, :duration
+  validates_presence_of :date, :aircraft_type, :from, :to, :num_landings, :duration
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,35 @@
-user = User.create(username: "kjoewill", email: "kjoewill@gmail.com", password: "password")
-user.tweets <<  Tweet.create(content: "Kev's best tweet ever!")
-user.tweets <<  Tweet.create(content: "Kev's second best tweet ever!")
-user.tweets <<  Tweet.create(content: "Kev's third best tweet ever!")
+kevin = User.create(username: "Kevin", password: "Kevin")
 
-user = User.create(username: "tdorwill", email: "tdorwill@gmail.com", password: "password")
-user.tweets <<  Tweet.create(content: "Tanya's best tweet ever!")
-user.tweets <<  Tweet.create(content: "Tanya's second best tweet ever!")
-user.tweets <<  Tweet.create(content: "Tanya's third best tweet ever!")
+kevin.flight_records << FlightRecord.create(
+  date: Date.today - 5, aircraft_type: "DA20", from: "KFLY", to: "KCOS", remarks: "Fun flight", num_landings: 1, duration: 60)
+
+kevin.flight_records << FlightRecord.create(
+  date: Date.today - 4, aircraft_type: "DA20", from: "KFLY", to: "KCOS", remarks: "Fun flight", num_landings: 1, duration: 60)
+
+kevin.flight_records << FlightRecord.create(
+  date: Date.today - 3, aircraft_type: "DA20", from: "KFLY", to: "KCOS", remarks: "Fun flight", num_landings: 1, duration: 60)
+
+kevin.flight_records << FlightRecord.create(
+  date: Date.today - 1, aircraft_type: "DA20", from: "KFLY", to: "KCOS", remarks: "Fun flight", num_landings: 1, duration: 60)
+
+kevin.flight_records << FlightRecord.create(
+  date: Date.today, aircraft_type: "DA20", from: "KFLY", to: "KCOS", remarks: "Fun flight", num_landings: 1, duration: 60)
+
+
+
+tanya = User.create(username: "Tanya", password: "Tanya")
+
+tanya.flight_records << FlightRecord.create(
+  date: Date.today - 5, aircraft_type: "SR20", from: "KFLY", to: "KCOS", remarks: "Fun flight", num_landings: 1, duration: 60)
+
+tanya.flight_records << FlightRecord.create(
+  date: Date.today - 4, aircraft_type: "SR20", from: "KFLY", to: "KCOS", remarks: "Fun flight", num_landings: 1, duration: 60)
+
+tanya.flight_records << FlightRecord.create(
+  date: Date.today - 3, aircraft_type: "SR20", from: "KFLY", to: "KCOS", remarks: "Fun flight", num_landings: 1, duration: 60)
+
+tanya.flight_records << FlightRecord.create(
+  date: Date.today - 1, aircraft_type: "SR20", from: "KFLY", to: "KCOS", remarks: "Fun flight", num_landings: 1, duration: 60)
+
+tanya.flight_records << FlightRecord.create(
+  date: Date.today, aircraft_type: "SR20", from: "KFLY", to: "KCOS", remarks: "Fun flight", num_landings: 1, duration: 60)
