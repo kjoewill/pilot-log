8b664f84c723180a5df133cb2a076353ed9444b8
github username <github email address>
2018-11-20 14:18:34 -0700

    Added more validations to flight_record model and deleted unneeded index page erb

--- app/models/flight_record.rb
+++ app/models/flight_record.rb
@@ -1,5 +1,7 @@
 class FlightRecord < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :date, :aircraft_type, :from, :to, :num_landings, :duration
+  validates :num_landings, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
+  validates :duration, numericality: { only_integer: true, :greater_than_or_equal_to => 1 }
 
 end
