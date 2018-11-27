159242961f91ca57d4f93d930977d40b55aa62ad
github username <github email address>
2018-11-27 14:35:03 -0700

    Removed some dead code.  Started section on user show page to display stats

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,4 @@ class User < ActiveRecord::Base
   validates_presence_of :username
   validates_uniqueness_of :username
 
-  def slug
-    self.username.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-  end
-
-  def self.find_by_slug(slug)
-    self.all.find {|e| e.slug == slug}
-  end
-
 end
